e273116343fa0b381d5c7afb5ceeba1dc46fee57
lordrichard6 <paulolopesreizinho@gmail.com>
2021-10-19 14:18:10 +0200

    inicial commit

---
+++ components/Header/index.tsx
@@ -0,0 +1,43 @@
+import styled from 'styled-components'
+
+export function Header() {
+    return (
+        <SectionContainer>
+            <SloganContainer>
+                <Title>
+                    “Because driving driving<br/>
+                    from A to B doesn’t have to be boring”
+                </Title>
+                <SubTitle>
+                    A driving service with a more friendly approach and a touch of humor.
+                </SubTitle>
+            </SloganContainer>
+
+        </SectionContainer>
+    )
+}
+
+const SectionContainer = styled.div`  
+    display: flex;
+    flex-direction: column;
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(259.91deg, #7F7FD5 -2.37%, #86A8E7 50.2%, #91EAE4 104.95%);
+`
+
+const SloganContainer = styled.div`  
+    display: flex;
+    flex-direction: column;
+`
+
+const Title = styled.h1`
+  font-size: 3.5rem;
+  font-weight: normal;
+  color: #FFFFFF;
+`
+
+const SubTitle = styled.h2`
+  font-size: 2rem;
+  font-weight: normal;
+  color: #FFFFFF;
+`
