4f72709b14c61dd3d6ef808d9086e870e5bd20b3
lordrichard6 <paulolopesreizinho@gmail.com>
2021-10-30 11:08:58 +0200

    header some corrections

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -27,14 +27,14 @@ export default function Header() {
 }
 
 const SectionContainer = styled.div`  
+    margin: 0;
     display: flex;
     flex-direction: column;
     justify-content: center;
     width: 100%;
-    height: 100vh;
+    min-height: 100vh;
     padding-left: 8rem;
     padding-right: 8rem;
-    /* background: linear-gradient(259.91deg, #7F7FD5 -2.37%, #86A8E7 50.2%, #91EAE4 104.95%); */
 `
 
 const SloganContainer = styled.div`  
@@ -44,13 +44,13 @@ const SloganContainer = styled.div`
 `
 
 const Title = styled.h1`
-  font-size: 3.5rem;
+  font-size: 3rem;
   font-weight: normal;
   color: #FFFFFF;
 `
 
 const SubTitle = styled.h2`
-  font-size: 2rem;
+  font-size: 1.5rem;
   font-weight: normal;
   color: #FFFFFF;
 `
