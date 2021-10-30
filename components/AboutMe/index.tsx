2f21ee722708477d4b207e4d4a72d02e06dfd055
lordrichard6 <paulolopesreizinho@gmail.com>
2021-10-30 10:54:12 +0200

    about me some corrections

--- components/AboutMe/index.tsx
+++ components/AboutMe/index.tsx
@@ -20,10 +20,19 @@ export default function AboutMe() {
                     <br />
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Iaculis tellus, faucibus volutpat fusce consequat in.
                 </p>
+                <Image
+                    src={picture}
+                    height={200}
+                    width={400}
+                    objectFit='cover'
+                />
             </LeftContainer>
             <RightContainer>
-                <RightImage
+                <Image
                     src={picture}
+                    height={600}
+                    width={600}
+                    objectFit='cover'
                 />
                 <h3>
                     “I have a passion to drive and social interaction and i think the current available services are overrated. Therefore, i decided
@@ -39,7 +48,7 @@ export default function AboutMe() {
 
 const SectionContainer = styled.div`  
     width: 80%;
-    height: 100vh;
+    min-height: 100vh;
     padding-left: 8rem;
     padding-right: 8rem;
     display: flex;
@@ -53,6 +62,7 @@ const LeftContainer = styled.div`
     height: 100%;
     margin-right: 0.5rem;
     h1 {
+        font-size: 2.5rem;
         font-weight: normal;
     }
 `
@@ -65,11 +75,12 @@ const RightContainer = styled.div`
     margin-left: 0.5rem;
     h3 {
         font-weight: normal;
+        font-size: 1.5rem;
     }
 
 `
 
-const RightImage = styled(Image)` 
-    width: 100%;
-    height: 40%;
-`
+// const RightImage = styled(Image)` 
+//     width: 100%;
+//     height: 40%;
+// `
