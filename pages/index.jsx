9660b2ddc1187ef0abaa17c20183c1b922fca915
lordrichard6 <paulolopesreizinho@gmail.com>
2021-12-16 19:08:03 +0100

    goggle search console and some meta tags added

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,8 +7,16 @@ export default function Home() {
     <>
       <Head>
         <title>Patricia Vila Nova</title>
-        <meta name="Patricia Vila Nova" content="Patricia Vila Nova" />
+        <meta name="Sardinha Drive" content="Chauffeur Service" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Sardinha Drive Chauffeur" key="title" />
+        <meta name="google-site-verification" content="KLB5GYXUpNq1bofeRXlUSDrmVZS0iXDGTNcigOJ7yfw" />
+        <meta name="theme-color" content="#085467" />
         <link rel="icon" href="/logo_noLetter.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/logo_noLetter.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/logo_noLetter.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/logo_noLetter.png" />
       </Head>
       <Homepage />
     </>
